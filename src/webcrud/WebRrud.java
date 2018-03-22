79662c5108e4926da78dbc00412113ffb80196c3
huangrong <huangrong@qq.com>
2018-03-22 15:52:15 +0800

    第一回合

---
+++ src/webcrud/WebRrud.java
@@ -0,0 +1,12 @@
+package webcrud;
+
+public class WebRrud {
+
+	public static void main(String[] args) {
+		System.out.println("hehe");
+		System.out.println("haha");
+		System.out.println("xixi");
+
+	}
+
+}
